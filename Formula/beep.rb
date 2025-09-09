e8e69a3d63a12d7b16c70c920116340cd825e448
adrianzavis <adrian@anstudio.sk>
2025-09-09 16:17:31 +0200

    Fixed version

--- Formula/beep.rb
+++ Formula/beep.rb
@@ -1,8 +1,8 @@
 class Beep < Formula
   desc "Simple macOS beep command"
-  homepage "https://github.com/YOURNAME/beep"
-  url "https://github.com/YOURNAME/beep/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "PUT_THE_SHA256_OF_TARBALL_HERE"
+  homepage "https://github.com/adianzavis/claude-beep"
+  url "https://github.com/YOURNAME/beep/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
   def install
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # claude-beep
+curl -L https://github.com/adianzavis/claude-beep/archive/refs/tags/v1.0.2.tar.gz | shasum -a 256
