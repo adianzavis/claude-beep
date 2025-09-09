75b0b0be0b91d793481c121037d71122885af36a
adrianzavis <adrian@anstudio.sk>
2025-09-09 16:06:19 +0200

    Initial formula

---
+++ Formula/beep.rb
@@ -0,0 +1,15 @@
+class Beep < Formula
+  desc "Simple macOS beep command"
+  homepage "https://github.com/YOURNAME/beep"
+  url "https://github.com/YOURNAME/beep/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "PUT_THE_SHA256_OF_TARBALL_HERE"
+  license "MIT"
+
+  def install
+    bin.install "bin/beep"
+  end
+
+  test do
+    system "#{bin}/beep", "1"
+  end
+end
