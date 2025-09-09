c363c5106be249391243c21b048b5ad87b131d93
adrianzavis <adrian@anstudio.sk>
2025-09-09 16:24:17 +0200

    beep 1.0.3

--- Formula/beep.rb
+++ Formula/beep.rb
@@ -1,9 +1,11 @@
 class Beep < Formula
   desc "Simple macOS beep command"
   homepage "https://github.com/adianzavis/claude-beep"
-  url "https://github.com/YOURNAME/beep/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  url "https://github.com/adianzavis/claude-beep/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "98e653180b18f07176b59b4e960dbef4c855d0c89206e5e85db9ff1a8aa61f7c"
   license "MIT"
+
+  head "https://github.com/adianzavis/claude-beep.git", branch: "main"
 
   def install
     bin.install "bin/beep"
