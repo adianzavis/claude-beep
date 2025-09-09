67af2f3b674eb7f1aba165c57763ae0bf5bdcb6d
adrianzavis <adrian@anstudio.sk>
2025-09-09 18:32:35 +0200

    Update beep formula to v1.0.4

--- Formula/beep.rb
+++ Formula/beep.rb
@@ -1,8 +1,8 @@
 class Beep < Formula
   desc "Simple macOS beep command"
   homepage "https://github.com/adianzavis/claude-beep"
-  url "https://github.com/adianzavis/claude-beep/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "98e653180b18f07176b59b4e960dbef4c855d0c89206e5e85db9ff1a8aa61f7c"
+  url "https://github.com/adianzavis/claude-beep/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "ce99acd7ebcc5e7f067bc62fc31df8f837b6c46caa79bf0d6b619e8a5a451b1c"
   license "MIT"
 
   head "https://github.com/adianzavis/claude-beep.git", branch: "main"
